706dc01d4f209c2adad2c4e792778348a1541e89
Kaider <kronoz@yandex.ru>
2024-12-14 19:46:29 +0300

    Прописал наименование дней недели полностью

--- Tracker/Data/WeekDay.swift
+++ Tracker/Data/WeekDay.swift
@@ -18,13 +18,13 @@ enum WeekDay: Int, CaseIterable {
     
     var shortName: String {
         switch self {
-        case .monday: return "Пн"
-        case .tuesday: return "Вт"
-        case .wednesday: return "Ср"
-        case .thursday: return "Чт"
-        case .friday: return "Пт"
-        case .saturday: return "Сб"
-        case .sunday: return "Вс"
+        case .monday: return "Понедельник"
+        case .tuesday: return "Вторник"
+        case .wednesday: return "Среда"
+        case .thursday: return "Четверг"
+        case .friday: return "Пятница"
+        case .saturday: return "Суббота"
+        case .sunday: return "Воскресенье"
         }
     }
 }
