83bab77a7cd2f4b2ca871ba943be0a3b6366b7b2
Kaider <kronoz@yandex.ru>
2025-01-05 23:08:08 +0300

    Сделал экраны выбора категорий по архитектуре MVVM. Привязал категории к CoreData.

--- Tracker/Protocols/TrackerCategoryStoreProtocol.swift
+++ Tracker/Protocols/TrackerCategoryStoreProtocol.swift
@@ -10,4 +10,5 @@ import Foundation
 protocol TrackerCategoryStoreProtocol {
     func createCategory(_ category: TrackerCategory) throws
     func fetchCategories() throws -> [TrackerCategory]
+    func deleteCategory(title: String) throws 
 }
